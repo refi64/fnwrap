4db0355961a33f4a3607bc6989e11c0507395dcb
Ryan Gonzalez <rymg19@gmail.com>
2017-04-25 13:34:24 -0500

    Add some unit tests

--- tst.cpp
+++ tst.cpp
@@ -1,21 +1,60 @@
 #include "fnwrap.hpp"
-#include <iostream>
+
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+#include "catch.hpp"
 
 
 int f() { return 0; }
-void g(int x, char y) {}
+int g(int x) { return x; }
+void h(int x, int y) {}
 
 
-void before(int n) { std::cout << "before: " << n << '\n'; }
-void after(int n) { std::cout << "after: " << n << '\n'; }
+int global=-1;
+
+void before(int n) { global = n; }
+void after(int n) { global += n; }
 
 
-FNWRAP(f, w_f, before(0), after(0))
-FNWRAP(g, w_g, before(a0), after(a0))
+FNWRAP(f, basic_f,,)
+FNWRAP(g, basic_g,,)
+FNWRAP(h, basic_h,,)
 
-int main() {
-    std::cout << "result: " << w_f() << '\n';
-    w_g(10, 11);
-    w_g(11, 12);
-    return 0;
+FNWRAP(f, before_f, before(2),)
+FNWRAP(g, before_g, before(a0),)
+FNWRAP(h, before_h, before(a1),)
+
+FNWRAP(f, before_after_f, before(2), after(10))
+FNWRAP(g, before_after_g, before(a0), after(a0))
+FNWRAP(h, before_after_h, before(a1), after(a0))
+
+
+TEST_CASE("The basics work", "[basics]") {
+    CHECK(basic_f() == 0);
+    CHECK(basic_g(1) == 1);
+    basic_h(1, 2);
+}
+
+
+TEST_CASE("before gets called", "[before]") {
+    before_f();
+    CHECK(global == 2);
+
+    before_g(10);
+    CHECK(global == 10);
+
+    before_h(12, 36);
+    CHECK(global == 36);
+}
+
+
+TEST_CASE("after gets called", "[after]") {
+    before_after_f();
+    CHECK(global == 12);
+
+    before_after_g(10);
+    CHECK(global == 20);
+
+    before_after_h(12, 36);
+    CHECK(global == 48);
 }
